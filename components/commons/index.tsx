787ae649bfdf936ab59c0af460289cc1b3588390
teknixproject <project@teknixcorp.com>
2025-06-19 16:53:21 +0700

    Update index - Add CustomWidget4b395dfd97c9

--- components/commons/index.tsx
+++ components/commons/index.tsx
@@ -19,4 +19,5 @@ export const components = {
   Table: dynamic(() => import('./Table')),
   LoginForm: dynamic(() => import('./LoginForm')),
   ListDrivers: dynamic(() => import('./ListDrivers')),
+  CustomWidget4b395dfd97c9: dynamic(() => import("./CustomWidget4b395dfd97c9")),
 };
